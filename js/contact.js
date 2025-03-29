bdf23387366e9ed7f1e4f7716cd8173886530ae2
Brener Sousa <53789728+Brenerss@users.noreply.github.com>
2025-03-29 15:56:25 -0300

    Delete js/contact.js

--- js/contact.js
+++
@@ -1,9 +0,0 @@
-const sendMessage = () => {
-    const element = document.getElementById('toast-container');
-    element.style.display = 'flex';
-}
-
-const closeMessage = () => {
-    const element = document.getElementById('toast-container');
-    element.style.display = 'none';
-}
