ff9e13ebbe8b0d800fa8c2a886ff2c00f5864e71
Brener Sousa <53789728+Brenerss@users.noreply.github.com>
2025-03-29 15:39:13 -0300

    Add files via upload

---
+++ js/contact.js
@@ -0,0 +1,9 @@
+const sendMessage = () => {
+    const element = document.getElementById('toast-container');
+    element.style.display = 'flex';
+}
+
+const closeMessage = () => {
+    const element = document.getElementById('toast-container');
+    element.style.display = 'none';
+}
--- js/menu.js
+++ js/menu.js
@@ -1,1 +1,15 @@
+const toggleMenu = () => {
+    const menu = document.getElementById('menu');
+    const [xIcon] = document.getElementsByClassName('x-icon');
+    const [menuIcon] = document.getElementsByClassName('menu-icon');
 
+    if (menu.style.display === 'block') {
+        menu.style.display = 'none';
+        xIcon.style.display = 'none';
+        menuIcon.style.display = 'block';
+    } else {
+        menu.style.display = 'block';
+        xIcon.style.display = 'block';
+        menuIcon.style.display = 'none';
+    }
+}
